7ec63f5b41141d428f66dda6350c287a40015c85
Jake Edgar <jakeedgar1012@gmail.com>
2021-10-26 12:59:20 -0700

    refine style and debug convertor

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,18 @@
-const ounces = 16
+//business logic//
 
 function multiply(number1, ounces) {
   return number1 * ounces;
 }
 
+//UI logic// 
+
 $(document).ready(function() {
   $("form#gallon").submit(function(e) {
     e.preventDefault();
+    const ounces = 16
     const number1 = parseInt($("#gal1").val());
     const result = multiply(number1, ounces);
     $("#output").text(result);
   }); 
+
 });
