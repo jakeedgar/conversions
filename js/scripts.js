85e52f53a8d93b827ace336cef1e3f3670927207
Jake Edgar <jakeedgar1012@gmail.com>
2021-10-26 11:10:25 -0700

    add first conversion and style choices

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,14 @@
-function divide(number1, number2) {
-  return number1 / number2;
+const ounces = 16
+
+function multiply(number1, ounces) {
+  return number1 * ounces;
 }
+
+$(document).ready(function() {
+  $("form#gallon").submit(function(e) {
+    e.preventDefault();
+    const number1 = parseInt($("#gal1").val());
+    const result = multiply(number1, ounces);
+    $("#output").text(result);
+  }); 
+});
