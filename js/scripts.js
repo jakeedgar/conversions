500d602d5c4ce15b692a0589ff57b286b34f0ca2
Jake Edgar <jakeedgar1012@gmail.com>
2021-10-26 13:21:15 -0700

    add second conversion

--- js/scripts.js
+++ js/scripts.js
@@ -4,14 +4,26 @@ function multiply(number1, ounces) {
   return number1 * ounces;
 }
 
+function multiply(number1, ounces2) {
+  return number1 * ounces2;
+}
+
 //UI logic// 
 
 $(document).ready(function() {
   $("form#gallon").submit(function(e) {
     e.preventDefault();
-    const ounces = 16
+    const ounces = 128
     const number1 = parseInt($("#gal1").val());
     const result = multiply(number1, ounces);
+    $("#output").text(result);
+  }); 
+
+  $("form#cups").submit(function(e) {
+    e.preventDefault();
+    const ounces2 = 8
+    const number1 = parseInt($("#cup1").val());
+    const result = multiply(number1, ounces2);
     $("#output").text(result);
   }); 
 
